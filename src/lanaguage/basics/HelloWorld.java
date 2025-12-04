6d83d986f1fbf7866f9387f9057cabdcbef1e133
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2025-12-04 19:36:54 +0530

    Java Intro

---
+++ src/lanaguage/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package lanaguage.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!"); //Sysout+Control+Space  //Control+Click ==> background code
+
+	}
+
+}
